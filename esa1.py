3e6d70795da3e64816ef5ed1c0b592d14536b7e3
k3v1n <k3v1n0x90@gmail.com>
2018-04-08 01:25:42 +0200

    Refactor the directory walker to use generators and calculate the hashcode of the file contents

--- esa1.py
+++ esa1.py
@@ -13,33 +13,88 @@ Options:
 
 __author__ = 'sattelmaier'
 
+import sys
 from hashlib import md5
 from os import listdir, path
-from docopt import docopt
+from collections import namedtuple
 
+def get_file_hash(filePath):
+    """
+    Get the md5 hash for the contents of a given file.
 
-def get_files_path_md5(abs_path, rel_path=''):
-    get_path = lambda ref, x: ref + '/' + x
-    get_abs_path = lambda x: get_path(abs_path, x)
-    get_rel_path = lambda x: get_path(rel_path, x)
+    Parameters
+    ----------
+    filePath : str
+
+    Returns
+    -------
+    str
+        The calculated md5 hashcode in hexadecimal format.
+    """
     md5_hash = md5()
 
     try:
-        dir_list = listdir(abs_path)
+        with open(filePath, 'rb') as fileHandle:
+            while True:
+                chunk = fileHandle.read(2048)
+
+                if not chunk:
+                    break
+
+                md5_hash.update(chunk)
+    except PermissionError as accessErr:
+        print("No permission to read file: {}. {}".format(filePath, accessErr), file=sys.stderr)
+        
+    return md5_hash.hexdigest()
+
+FileInfo = namedtuple('FileInfo', ['file_name', 'relative_path', 'hash_code'])
+
+def iterate_directory(directoryPath):
+    """
+    Iterate the contents of a directory recursively and collect file information.
+
+    Parameters
+    ----------
+    directoryPath : str
+
+    Yields
+    ------
+    FileInfo
+        A 3 tuple with file name, relative path and the calculated hash code.
+
+    Raises
+    ------
+    OSError
+        If a problem occurs during directory access that is not permission related.
+    """
+
+    directoryItems = []
+
+    try:
+        directoryItems = listdir(directoryPath)
+    except PermissionError as accessErr:
+        print("No permission to open directory: {}. {}".format(directoryPath, accessErr), file=sys.stderr)
     except OSError as err:
-        print("OS error: {0}".format(err))
-        return
+        print("OS error: {}".format(err), file=sys.stderr)
+        raise
 
-    for element in dir_list:
-        element_encoded = element.encode('utf-8')
-        md5_hash.update(element_encoded)
-        md5_hash_hex = md5_hash.hexdigest()
-        print("Filename: {}, Path: {}, MD5: {})".format(element, get_rel_path(element), md5_hash_hex))
+    for item in directoryItems:
+        fullItemPath = path.join(directoryPath, item)
 
-        if path.isdir(get_abs_path(element)):
-            get_files_path_md5(get_abs_path(element), get_rel_path(element))
+        if path.isfile(fullItemPath):
 
+            hashCode = get_file_hash(fullItemPath)
+            fileInfo = FileInfo(item, path.relpath(directoryPath), hashCode)
+            yield fileInfo
+
+        elif path.isdir(fullItemPath):
+
+            for fileInfo in iterate_directory(fullItemPath):
+                yield fileInfo
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version='eas1 1.0')
-    get_files_path_md5(arguments['<path>'])
+
+    directoryPath = sys.argv[1]
+
+    for fileInfo in iterate_directory(directoryPath):
+        print("Filename: {}, Path: {}, Hash: {}".format(fileInfo.file_name, fileInfo.relative_path, fileInfo.hash_code))
